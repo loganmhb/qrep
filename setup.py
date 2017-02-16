d4be706dfd89bc362ef621825519e786c89e586e
Logan Buckley <logan.buckley@gmail.com>
2017-02-15 19:50:04 -0500

    Set up repo for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='qrep',
+    packages=['qrep'],
+    version='0.1.1',
+    description='Interactive query-replace',
+    author='Logan Buckley',
+    author_email='logan.buckley@gmail.com',
+    url='https://github.com/loganmhb/qrep',
+    download_url='https://github.com/loganmhb/qrep/tarball/0.1.1',
+    keywords=['cli', 'query', 'replace', 'sed'],
+    classifiers=[]
+)
