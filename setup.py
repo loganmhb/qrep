4fac4493fc21aef0d4b766abe6f4bf84a7200fe6
Logan Buckley <logan.buckley@gmail.com>
2017-02-15 20:10:17 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='qrep',
     packages=['qrep'],
-    version='0.1.1',
+    version='0.1.2',
     description='Interactive query-replace',
     author='Logan Buckley',
     author_email='logan.buckley@gmail.com',
